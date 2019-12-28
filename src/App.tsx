2cfe20508f3de41b170fc2842f33c23247f08fdd
robiharid <robiharid@gmail.com>
2019-12-28 01:03:43 +0000

    initial html outline

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import './App.css';
 
+import HomePage from './homepage.component';
+
 const App: React.FC = () => {
-	return <div>Hello World</div>;
+	return (
+		<div>
+			<HomePage />>
+		</div>
+	);
 };
 
 export default App;
